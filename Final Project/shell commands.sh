eaeb2c93b99747bf87cb9b3fa85cefc8c76b2d42
Jixiao Yang <yang.jix@husky.neu.edu>
2019-12-13 04:19:44 -0500

    update

--- Final Project/shell commands.sh
+++ Final Project/shell commands.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Hadoop: Number of Businesses in each city and state
-hadoop jar Hadoop-1.0-SNAPSHOT.jar Business.NumersInEachCityAndState.MapReduce /yelp/business.json /fpresults/hadoop/NumberOfBusinessesEachCityAndState
+hadoop jar Hadoop/target/Hadoop-1.0-SNAPSHOT.jar Business.NumersInEachCityAndState.MapReduce /yelp/business.json /fpresults/hadoop/NumberOfBusinessesEachCityAndState
 # Hadoop: Top 5 Rated Business in each state
-hadoop jar Hadoop-1.0-SNAPSHOT.jar Business.Top5RatedBusinessInEachState.MapReduce /yelp/business.json /fpresults/hadoop/Top5RatedBusinessInEachState
+hadoop jar Hadoop/target/Hadoop-1.0-SNAPSHOT.jar Business.Top5RatedBusinessInEachState.MapReduce /yelp/business.json /fpresults/hadoop/Top5RatedBusinessInEachState
 # Hadoop: Perentage of Ratings
-hadoop jar Hadoop-1.0-SNAPSHOT.jar Review.PercentageOfRatings.MapReduce /yelp/review.json /fpresults/hadoop/PercentageOfRatings
+hadoop jar Hadoop/target/Hadoop-1.0-SNAPSHOT.jar Review.PercentageOfRatings.MapReduce /yelp/review.json /fpresults/hadoop/PercentageOfRatings
 # Hadoop: Moving average rating of each business
-hadoop jar Hadoop-1.0-SNAPSHOT.jar Review.MovingAverageRatingOfEachBusiness.MapReduce /yelp/review.json /fpresults/hadoop/MovingAverageRatingOfEachBusiness
+hadoop jar Hadoop/target/Hadoop-1.0-SNAPSHOT.jar Review.MovingAverageRatingOfEachBusiness.MapReduce /yelp/review.json /fpresults/hadoop/MovingAverageRatingOfEachBusiness
 # HBase: Perentage of Ratings
-hadoop jar HBase-1.0-SNAPSHOT.jar Review.PercentageOfRatings.MapReduce
+hadoop jar HBase/target/HBase-1.0-SNAPSHOT.jar Review.PercentageOfRatings.MapReduce
 
